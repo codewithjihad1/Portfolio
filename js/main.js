02a2ffbf7430566f6efafb2df1c0b21b1d2784e0
MD JIHAD HOSSAIN <89403684+jihad100@users.noreply.github.com>
2022-02-11 14:21:17 +0600

    create buttom hover effect

--- js/main.js
+++ js/main.js
@@ -1,46 +1,46 @@
-// Navigation menu
-let body = document.querySelector("body");
-let menuBtn = document.querySelector(".menu-button");
-let canceBtn = document.querySelector(".cancel-btn");
-let navBar = document.querySelector(".navbar");
+// // Navigation menu
+// let body = document.querySelector("body");
+// let menuBtn = document.querySelector(".menu-button");
+// let canceBtn = document.querySelector(".cancel-btn");
+// let navBar = document.querySelector(".navbar");
 
-menuBtn.onclick = function() {
-  menuBtn.style.opacity = '0';
-  menuBtn.style.pointerEvents = 'none';
-  navBar.classList.add("active");
-}
-canceBtn.onclick = function() {
-  menuBtn.style.opacity = '1';
-  menuBtn.style.pointerEvents = 'auto';
-  navBar.classList.remove("active");
-}
+// menuBtn.onclick = function() {
+//   menuBtn.style.opacity = '0';
+//   menuBtn.style.pointerEvents = 'none';
+//   navBar.classList.add("active");
+// }
+// canceBtn.onclick = function() {
+//   menuBtn.style.opacity = '1';
+//   menuBtn.style.pointerEvents = 'auto';
+//   navBar.classList.remove("active");
+// }
 
-// Sticky navb
-let val;
-let nav = document.querySelector("nav");
-let scrollBtn = document.querySelector(".scroll-button a");
-console.log(scrollBtn);
+// // Sticky navb
+// let val;
+// let nav = document.querySelector("nav");
+// let scrollBtn = document.querySelector(".scroll-button a");
+// console.log(scrollBtn);
 
-  window.onscroll = function(){
-    if (document.documentElement.scrollTop > 20) {
-      nav.classList.add("sticky");
-      scrollBtn.style.display = "block";
-    }else{
-      nav.classList.remove("sticky");
-      scrollBtn.style.display = "none";
-    }
-  }
+//   window.onscroll = function(){
+//     if (document.documentElement.scrollTop > 20) {
+//       nav.classList.add("sticky");
+//       scrollBtn.style.display = "block";
+//     }else{
+//       nav.classList.remove("sticky");
+//       scrollBtn.style.display = "none";
+//     }
+//   }
 
 
-// Side Navigation Bar Close While We Click On Navigation Links
-let navLinks = document.querySelectorAll(".menu li a");
-for (var i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener("click" , function() {
-    navBar.classList.remove("active");
-    menuBtn.style.opacity = "1";
-    menuBtn.style.pointerEvents = "auto";
-  });
-}
+// // Side Navigation Bar Close While We Click On Navigation Links
+// let navLinks = document.querySelectorAll(".menu li a");
+// for (var i = 0; i < navLinks.length; i++) {
+//   navLinks[i].addEventListener("click" , function() {
+//     navBar.classList.remove("active");
+//     menuBtn.style.opacity = "1";
+//     menuBtn.style.pointerEvents = "auto";
+//   });
+// }
 
 
 // Vanilla tilt Effects
@@ -49,3 +49,15 @@ VanillaTilt.init(document.querySelectorAll(".header-social ul li a"), {
   speed: 400,
   scale: 1.2
 });
+
+// Button Hover Effects
+const btn = document.querySelector(".button");
+
+btn.onmousemove = function(e) {
+    const x = e.pageX - btn.offsetLeft;
+    const y = e.pageY - btn.offsetTop;
+
+    btn.style.setProperty('--x', x + 'px');
+    btn.style.setProperty('--y', y + 'px');
+}
+
