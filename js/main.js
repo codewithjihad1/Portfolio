bcc3c83a0b173a1026fa60f75563ceeb9b85e013
unknown <mdjihadhossain0011@gmail.com>
2022-02-28 11:47:08 +0600

    button hover effect add

--- js/main.js
+++ js/main.js
@@ -1,63 +1,22 @@
-// // Navigation menu
-// let body = document.querySelector("body");
-// let menuBtn = document.querySelector(".menu-button");
-// let canceBtn = document.querySelector(".cancel-btn");
-// let navBar = document.querySelector(".navbar");
-
-// menuBtn.onclick = function() {
-//   menuBtn.style.opacity = '0';
-//   menuBtn.style.pointerEvents = 'none';
-//   navBar.classList.add("active");
-// }
-// canceBtn.onclick = function() {
-//   menuBtn.style.opacity = '1';
-//   menuBtn.style.pointerEvents = 'auto';
-//   navBar.classList.remove("active");
-// }
-
-// // Sticky navb
-// let val;
-// let nav = document.querySelector("nav");
-// let scrollBtn = document.querySelector(".scroll-button a");
-// console.log(scrollBtn);
-
-//   window.onscroll = function(){
-//     if (document.documentElement.scrollTop > 20) {
-//       nav.classList.add("sticky");
-//       scrollBtn.style.display = "block";
-//     }else{
-//       nav.classList.remove("sticky");
-//       scrollBtn.style.display = "none";
-//     }
-//   }
-
-
-// // Side Navigation Bar Close While We Click On Navigation Links
-// let navLinks = document.querySelectorAll(".menu li a");
-// for (var i = 0; i < navLinks.length; i++) {
-//   navLinks[i].addEventListener("click" , function() {
-//     navBar.classList.remove("active");
-//     menuBtn.style.opacity = "1";
-//     menuBtn.style.pointerEvents = "auto";
-//   });
-// }
-
 
 // Vanilla tilt Effects
 VanillaTilt.init(document.querySelectorAll(".header-social ul li a"), {
-  max: 40,
-  speed: 400,
-  scale: 1.2
+    max: 40,
+    speed: 400,
+    scale: 1.2
 });
 
 // Button Hover Effects
-const btn = document.querySelector(".button");
+let ripple = function rippleEffect(e) {
+    const x = e.pageX - this.offsetLeft;
+    const y = e.pageY - this.offsetTop;
 
-btn.onmousemove = function(e) {
-    const x = e.pageX - btn.offsetLeft;
-    const y = e.pageY - btn.offsetTop;
-
-    btn.style.setProperty('--x', x + 'px');
-    btn.style.setProperty('--y', y + 'px');
+    this.style.setProperty('--x', x + 'px');
+    this.style.setProperty('--y', y + 'px');
 }
 
+const aboutBtn = document.querySelector(".button");
+aboutBtn.onmousemove = ripple;
+
+let heroBtn = document.querySelector('.hero-btn button');
+heroBtn.onmousemove = ripple;
