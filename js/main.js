6f27882f423ffc42c48ee6914c6b558f84ddaf7a
unknown <mdjihadhossain766@gmail.com>
2022-03-08 19:34:55 +0600

    navigation menu responsive & sticky

--- js/main.js
+++ js/main.js
@@ -23,3 +23,30 @@ heroBtn.onmousemove = ripple;
 
 let contactBtn = document.querySelector('.contact-btn button');
 contactBtn.onmousemove = ripple;
+
+
+// Navigation menu
+let body = document.querySelector("body");
+let menuBar = document.querySelector('.menu-button')
+let cancelBtn = document.querySelector(".cancel-btn")
+
+menuBar.addEventListener('click', () => {
+    body.classList.add("navShow");
+    menuBar.style.opacity = "0"
+})
+
+cancelBtn.addEventListener('click', () => {
+    body.classList.remove('navShow')
+    menuBar.style.opacity = "1"
+})
+
+
+// sticy nav
+let navbar = document.querySelector('nav');
+window.onscroll = function() {
+    if(document.documentElement.scrollTop > 20){
+        navbar.classList.add('sticky')
+    }else {
+        navbar.classList.remove('sticky')
+    }
+}
